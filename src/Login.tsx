6932fce29a0aa2059de160483e5c2eb6ccb872d1
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-09-19 11:12:52 +0200

    Update Login.tsx
    I added a div to style it as card

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Registration from './Registration';
 import { Link } from 'react-router-dom';
 
 function Login() {
@@ -7,73 +6,73 @@ function Login() {
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
+    e.preventDefault();
     console.log('Logging in with:', { username, password });
     alert('Logged in!');
   };
 
   return (
     <>
-    <div className="card-container">
-        
-               <header>
-                 <nav className="nav-container">
-                   <ul className="nav-left">
-                     <li><Link to="/Home">Home</Link></li>
-                     <li><Link to="/JobPage">Jobs</Link></li>
-                     <li><Link to="/">Landing Page</Link></li>
-                    
-                   </ul>
-                   <ul className="nav-right">
-                     <li><input type="text" placeholder="Search" /></li>
-                     <li></li>
-                     <li> Get the app</li>
-                     <li><Link to="/Registration">Sign Up</Link></li>
-                     <li><Link to="/Login">Sign in</Link></li>
-                   </ul>
-                 </nav>
-               </header>
+      <div className="card-container">
 
+        <header>
+          <nav className="nav-container">
+            <ul className="nav-left">
+              <li><Link to="/Home">Home</Link></li>
+              <li><Link to="/JobPage">Jobs</Link></li>
+              <li><Link to="/">Landing Page</Link></li>
 
-      <main>
-        <div>
-           <h2>Welcome!</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label>Username:</label>
-              <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="username"
-              required/>
+            </ul>
+            <ul className="nav-right">
+              <li><input type="text" placeholder="Search" /></li>
+              <li></li>
+              <li> Get the app</li>
+              <li><Link to="/Registration">Sign Up</Link></li>
+              <li><Link to="/Login">Sign in</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+           <div className="form-container">
+          <div>
+            <h2>Welcome!</h2>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label>Username:</label>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  placeholder="username"
+                  required />
+              </div>
+
+              <div>
+                <label>Password:</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="password"
+                  required />
+              </div>
+
+              <button type="submit">Log in</button>
+            </form>
+
+            <p>
+              Don't have an account?
+              <Link to="/Registration">Sign Up</Link>
+
+             
+            </p>
             </div>
+          </div>
 
-            <div>
-              <label>Password:</label>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="password"
-             required />
-            </div>
-
-            <button type="submit">Log in</button>
-          </form>
-
-          <p>
-            Don't have an account?
-            <button onClick={Registration}>
-              Sign Up
-            </button>
-          </p>
-        </div>
-
-      </main>
-      <footer>
-       <div></div>
-      </footer>
+        </main>
+        <footer>
+          <div></div>
+        </footer>
       </div>
     </>
   );
