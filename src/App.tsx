a65d766668f3d7e40829ca71cab2d2bf1ec89c67
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-09-19 14:04:33 +0200

    Update App.tsx
    I added BrowserRouter so you can be able to navigate through pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { BrowserRouter as   Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./LandingPage";
+import Login from "./Login";
 import Registration from "./Registration";
+import Home from "./Home";
+import JobPage from "./JobPage";
 
 
 function App() {
   return (
-  
-      <Registration  />
-  
+
+   <Router>
+     <Routes>
+      <Route path="/" element= {<LandingPage/>}/>
+      <Route path="/Login" element={<Login/>}/>
+      <Route path="/Registration" element={<Registration/>}/>
+      <Route path="/Home" element ={<Home/>}/>
+      <Route path="/JobPage" element={<JobPage/>}/>
+    </Routes>
+   </Router>
+   
   );
 }
 
