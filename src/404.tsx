e1b7c2827eca7626db3b865bf86283fbe4130e57
Mhangwani Ndima Karabo <203499810+Ndima-karabo17@users.noreply.github.com>
2025-09-22 10:51:43 +0200

    Update 404.tsx
    I added a header to be consistance with other page

--- src/404.tsx
+++ src/404.tsx
@@ -1,12 +1,30 @@
+
 import { Link } from 'react-router-dom';
 
 function NotFound() {
   return (
+    <>
+       <header>
+        <nav className="nav-container">
+          <ul className="nav-left">
+            <li><Link to="/Home">Home</Link></li>
+            <li><Link to="/JobPage">Jobs</Link></li>
+
+          </ul>
+          <ul className="nav-right">
+          
+          
+            <li><Link to="/Registration">Sign Up</Link></li>
+            <li><Link to="/Login">Sign in</Link></li>
+          </ul>
+        </nav>
+      </header>
     <div>
       <h1 style={styles.heading}>404 - Page Not Found</h1>
       <p style={styles.text}>Sorry, Page you are looking for is not available. Please check spelling </p>
       <Link to="/Home" style={styles.link}>Go back to Home</Link>
     </div>
+    </>
   );
 }
 
@@ -36,4 +54,3 @@ const styles = {
 };
 
 export default NotFound;
-
